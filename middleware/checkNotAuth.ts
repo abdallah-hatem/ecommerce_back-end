bb4282a01714cf9044e15f3647312e36f18b1308
abdallah-hatem <ahkortam@gmail.com>
2023-09-20 02:44:43 +0300

    test checkNotAuth

--- middleware/checkNotAuth.ts
+++ middleware/checkNotAuth.ts
@@ -5,5 +5,17 @@ export default function checkNotAuth(req: any, res: any, next: any) {
   if (!token) {
     return next();
   }
-  return res.status(401).json({ message: "Already logged in" });
+
+  if (token) {
+    jwt.verify(token, process.env.TOKEN_SECRET, callBack);
+  }
+
+  function callBack(err: any, decodedToken: any) {
+    if (err) {
+      // return res.status(401).json({ err });
+      return res.status(401).json({ message: "Already logged in" });
+    }
+    next();
+  }
+  // return res.status(401).json({ message: "Already logged in" });
 }
