60f3ecda2c78c9885a4acf1b9e7430290e4f77c4
abdallah-hatem <ahkortam@gmail.com>
2023-09-20 02:59:30 +0300

    test checkNotAuth 2

--- middleware/checkNotAuth.ts
+++ middleware/checkNotAuth.ts
@@ -2,20 +2,9 @@ const jwt = require("jsonwebtoken");
 
 export default function checkNotAuth(req: any, res: any, next: any) {
   const token = req.cookies.jwt;
-  if (!token) {
+
+  if (token === undefined) {
     return next();
   }
-
-  if (token) {
-    jwt.verify(token, process.env.TOKEN_SECRET, callBack);
-  }
-
-  function callBack(err: any, decodedToken: any) {
-    if (err) {
-      // return res.status(401).json({ err });
-      return res.status(401).json({ message: "Already logged in" });
-    }
-    next();
-  }
-  // return res.status(401).json({ message: "Already logged in" });
+  return res.status(401).json({ message: "Already logged in" });
 }
