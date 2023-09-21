7ec58cf34346e47e3da48ef61567455c80db0b5b
abdallah-hatem <ahkortam@gmail.com>
2023-09-21 17:48:59 +0300

    added dynamic db url to prisma

--- config/prisma.js
+++ config/prisma.js
@@ -1,4 +1,21 @@
 const { PrismaClient } = require("@prisma/client");
-const prisma = new PrismaClient();
+
+function dbUrl() {
+  if (process.env.NODE_ENV === "production") {
+    return {
+      url: process.env.POSTGRES_PRISMA_URL,
+    };
+  }
+
+  return {
+    url: process.env.DATABASE_URL,
+  };
+}
+
+const prisma = new PrismaClient({
+  datasources: {
+    db: dbUrl(),
+  },
+});
 
 module.exports = { prisma };
